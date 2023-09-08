b1c5b0f5d9982370a83364b37926f30fe2c8e2d2
Swetabh333 <swetabh333@gmail.com>
2023-09-08 20:54:24 +0530

    all csv added

--- routes/router12.js
+++ routes/router12.js
@@ -13,7 +13,7 @@ const BASE_URL = process.env.BASE_URL
 const router = express.Router()
 
 router.post('/',async(req,res)=>{
-   
+   console.log(req.body);
     if(req.body.table==='daily'){
         let date = new Date(req.body.Date);
         console.log(date);
@@ -52,6 +52,118 @@ router.post('/',async(req,res)=>{
             find.map((elem)=>{
                 csvStream.write({
                     date:elem.date?elem.date:'-',
+                    RO:elem.RO?elem.RO:'-',
+                    DCC:elem.DCC?elem.DCC:'-',
+                    DCA:elem.DCA?elem.DCA:'-',
+                    CCC:elem.CCC?elem.CCC:'-',
+                    CCA:elem.CCA?elem.CCA:'-',
+                    IBC:elem.IBC?elem.IBC:'-',
+                    IBA:elem.IBA?elem.IBA:'-',
+                    BHIMC:elem.BHIMC?elem.BHIMC:'-',
+                    BHIMA:elem.BHIMA?elem.BHIMA:'-',
+                    POSC:elem.POSC?elem.POSC:'-',
+                    POSA:elem.POSA?elem.POSA:'-',
+                    UPIC:elem.UPIC?elem.UPIC:'-',
+                    UPIA:elem.UPIA?elem.UPIA:'-'
+                })
+            })
+        }
+
+        csvStream.end()
+        writeableStream.end()
+        
+    }catch(err){
+            res.status(401).json(err)
+        }
+    }else if(req.body.table==='quarterly'){
+        try{
+            let find = await Quarterly.find({
+            QUARTER: parseInt(req.body.quarter)
+        })
+        console.log(find)
+        const csvStream = fastcsv.format({headers:true});
+
+        if(!fs.existsSync('public/files/export')){
+            if(!fs.existsSync('public/files')){
+                fs.mkdirSync('public/files/')
+            }
+            if(!fs.existsSync('public/files/export')){
+                fs.mkdirSync('./public/files/export')
+            }
+        }
+
+        const writeableStream = fs.createWriteStream(
+            "public/files/export/users.csv"
+        )
+
+        csvStream.pipe(writeableStream);
+
+        writeableStream.on('finish',()=>{
+            res.status(200).json({
+                downloadUrl:`${BASE_URL}/files/export/users.csv`
+            });
+        });
+
+        if(find.length >0){
+            find.map((elem)=>{
+                csvStream.write({
+                    quarter:elem.QUARTER?elem.QUARTER:'-',
+                    RO:elem.RO?elem.RO:'-',
+                    DCC:elem.DCC?elem.DCC:'-',
+                    DCA:elem.DCA?elem.DCA:'-',
+                    CCC:elem.CCC?elem.CCC:'-',
+                    CCA:elem.CCA?elem.CCA:'-',
+                    IBC:elem.IBC?elem.IBC:'-',
+                    IBA:elem.IBA?elem.IBA:'-',
+                    BHIMC:elem.BHIMC?elem.BHIMC:'-',
+                    BHIMA:elem.BHIMA?elem.BHIMA:'-',
+                    POSC:elem.POSC?elem.POSC:'-',
+                    POSA:elem.POSA?elem.POSA:'-',
+                    UPIC:elem.UPIC?elem.UPIC:'-',
+                    UPIA:elem.UPIA?elem.UPIA:'-'
+                })
+            })
+        }
+
+        csvStream.end()
+        writeableStream.end()
+        
+    }catch(err){
+            res.status(401).json(err)
+        }
+    }else{
+        try{
+            let find = await Yearly.find({
+            YEAR:req.body.year
+        })
+
+        const csvStream = fastcsv.format({headers:true});
+
+        if(!fs.existsSync('public/files/export')){
+            if(!fs.existsSync('public/files')){
+                fs.mkdirSync('public/files/')
+            }
+            if(!fs.existsSync('public/files/export')){
+                fs.mkdirSync('./public/files/export')
+            }
+        }
+
+        const writeableStream = fs.createWriteStream(
+            "public/files/export/users.csv"
+        )
+
+        csvStream.pipe(writeableStream);
+
+        writeableStream.on('finish',()=>{
+            res.status(200).json({
+                downloadUrl:`${BASE_URL}/files/export/users.csv`
+            });
+        });
+
+        if(find.length >0){
+            find.map((elem)=>{
+                csvStream.write({
+                    year:elem.YEAR?elem.YEAR:'-',
                     RO:elem.RO?elem.RO:'-',
                     DCC:elem.DCC?elem.DCC:'-',
                     DCA:elem.DCA?elem.DCA:'-',
